be0ebd280fe8c9f85859d5f76d8dbc6ec1d910dc
Kevin Eifinger <k.eifinger@googlemail.com>
2017-12-08 17:01:17 +0100

    Added say_hello and moved strings to config files

--- TravelAdvisor.py
+++ TravelAdvisor.py
@@ -1,3 +1,4 @@
+import configparser
 import os
 import time
 import threading
@@ -15,9 +16,9 @@ class TravelAdvisor:
 
     BOT_NAME = "travel-advisor"
     WATSON_CONVERSATION_VERSION = '2017-05-26'
+    MESSAGES_CONFIG_FILE = "messages.conf"
+    INTENTS_CONFIG_FILE = "intents.conf"
 
-    MESSAGE_READY = "Du kannst losfahren"
-    MESSAGE_CURRENT_TRAVEL = "Deine momentane Reisezeit beträgt: "
     CHECK_TRAVEL_DELAY = 60.0*2
     MAX_TRAVEL_CHECKS = 3600/CHECK_TRAVEL_DELAY
 
@@ -36,6 +37,8 @@ class TravelAdvisor:
             version= self.WATSON_CONVERSATION_VERSION
         )
         self.users = {}
+        self.messages = self._load_config(self.MESSAGES_CONFIG_FILE)
+        self.intents = self._load_config(self.INTENTS_CONFIG_FILE)
 
     def _get_bot_id(self):
         """
@@ -49,6 +52,13 @@ class TravelAdvisor:
             for user in users:
                 if 'name' in user and user.get('name') == self.BOT_NAME:
                     return user.get('id')
+
+    def _load_config(self, configFile):
+        parent_dir = os.path.dirname(__file__)
+        config_file = os.path.join(parent_dir, configFile)
+        parser = configparser.ConfigParser()
+        parser.read(config_file)
+        return parser
 
     def get_user_name(self, user_id):
         api_call = self.slack_client.api_call("users.list")
@@ -68,40 +78,45 @@ class TravelAdvisor:
         output_list = slack_rtm_output
         if output_list and len(output_list) > 0:
             for output in output_list:
+                print(output)
+                print(self.AT_BOT)
                 if output and 'text' in output and self.AT_BOT in output['text']:
                     # return text after the @ mention, whitespace removed
                     return output['text'].split(self.AT_BOT)[1].strip().lower(), \
-                           output['channel'], output['user']
+                           output['channel'], output['user'], True
                 else:
                     if output and 'channel' in output and output['channel'].startswith("D") \
                     and 'type' in output and output['type'] == 'message' \
                     and 'user' in output and output['user'] != self.BOT_ID:
-                        return output['text'], output['channel'], output['user']
+                        return output['text'], output['channel'], output['user'], False
 
-        return None, None, None
+        return None, None, None, None
 
-    def handle_command(self,command, user_id, channel):
+    def handle_command(self,command, user_id, channel, is_AT_bot):
         """
             Receives commands directed at the bot and determines if they
             are valid commands. If so, then acts on the commands. If not,
             returns back what it needs for clarification.
         """
-        travel_command = "Wann soll ich losfahren?"
-        response ="Tut mir leid, das verstehe ich noch nicht =("
-
-        intent = self.get_watson_intent(command)
-        if intent == "when_should_I_leave":
-            if user_id in self.users:
-                user = self.users.get(user_id)
-                if user.travel_request:
-                    self.already_existing_request(user, channel)
-            else:
-                new_user = User(user_id)
-                self.users[user_id] = new_user
-                new_user.name = self.get_user_name(user_id)
-                new_user.travel_request = TravelRequest(channel)
-                self.ask_for_origin(channel)
-        elif user_id in self.users:
+        if is_AT_bot:
+            intent = self.get_watson_intent(command)
+            if intent == self.intents.get("DEFAULT","SAY_HELLO"):
+                print("say hello intent detected")
+                self.send_message(eval(self.messages.get("DEFAULT","SAY_HELLO")), channel)
+        else:
+            intent = self.get_watson_intent(command)
+            if intent == self.intents.get("DEFAULT","WHEN_SHOULD_I_LEAVE"):
+                if user_id in self.users:
+                    user = self.users.get(user_id)
+                    if user.travel_request:
+                        self.already_existing_request(user, channel)
+                else:
+                    new_user = User(user_id)
+                    self.users[user_id] = new_user
+                    new_user.name = self.get_user_name(user_id)
+                    new_user.travel_request = TravelRequest(channel)
+                    self.ask_for_origin(channel)
+            elif user_id in self.users:
                 user = self.users.get(user_id)
                 if user.travel_request:
                     if not user.travel_request.origin:
@@ -111,34 +126,34 @@ class TravelAdvisor:
                         user.travel_request.destination = command
                         self.ask_for_target_duration(channel)
                     elif not user.travel_request.target_duration:
-                        user.travel_request.target_duration = int(command) *60
+                        user.travel_request.target_duration = int(command) * 60
                         self.handle_travel_request(user.travel_request, channel)
-        else:
-            self.send_message(response, channel)
+            else:
+                self.send_message(eval(self.messages.get("DEFAULT","UNKOWN_COMMAND")), channel)
 
     def ask_for_target_duration(self, channel):
-        response = "Ab wieviel Minuten oder weniger soll ich dir bescheid geben?"
+        response = eval(self.messages.get("DEFAULT","TARGET_DURATION"))
         self.send_message(response, channel)
 
     def ask_for_destination(self, channel):
-        response = "Danke. Wohin willst du fahren?"
+        response = eval(self.messages.get("DEFAULT","DESTINATION"))
         self.send_message(response, channel)
 
     def ask_for_origin(self,channel):
-        response = "Ich habe erkannt, dass du wissen willst wann du losfahren sollst. Von wo willst du losfahren?"
+        response = "{} {}".format(eval(self.messages.get("DEFAULT","DETECTED_WHEN_SHOULD_I_LEAVE")), eval(self.messages.get("DEFAULT","ORIGIN")))
         self.send_message(response, channel)
 
     def already_existing_request(self, user, channel):
-        response = "Du hast bereits einen bestehenden Auftrag. {}{}".format(self.MESSAGE_CURRENT_TRAVEL,user.travel_request.duration_in_traffic['text'])
+        response = "{}{}".format(eval(self.messages.get("DEFAULT","EXISTING_REQUEST")), eval(self.messages.get("DEFAULT","CURRENT_TRAVEL_TIME")),user.travel_request.duration_in_traffic['text'])
         self.send_message(response, channel)
 
     def handle_travel_request(self, request, channel):
         request.check_current_travel(self.gmaps)
 
-        message = "{}{}".format(self.MESSAGE_CURRENT_TRAVEL,request.duration_in_traffic['text'])
+        message = "{}{}".format(eval(self.messages.get("DEFAULT","CURRENT_TRAVEL_TIME")),request.duration_in_traffic['text'])
         self.send_message(message,channel)
 
-        message = "Ich gebe dir bescheid sobald die Reisezeit weniger als {} Minuten beträgt.".format(int(request.target_duration/60))
+        message = eval(self.messages.get("DEFAULT","I_WILL_NOTIFY")).format(int(request.target_duration/60))
         self.send_message(message, channel)
 
         self.check_travel_request(request)
@@ -151,7 +166,7 @@ class TravelAdvisor:
         else:
             request.check_current_travel(self.gmaps)
             if request.duration_in_traffic['value'] <= request.target_duration:
-                message = "{} nach {}".format(self.MESSAGE_READY,request.destination)
+                message = "{} nach {}".format(eval(self.messages.get("DEFAULT","YOU_CAN_LEAVE_NOW")),request.destination)
                 self.send_message(message, request.channel)
             else:
                 timer = threading.Timer(self.CHECK_TRAVEL_DELAY, self.check_travel_request, args=(request,))
@@ -193,9 +208,9 @@ if __name__ == "__main__":
     if bot.slack_client.rtm_connect():
         print("StarterBot connected and running!")
         while True:
-            command, channel, user = bot.parse_slack_output(bot.slack_client.rtm_read())
+            command, channel, user, is_AT_bot = bot.parse_slack_output(bot.slack_client.rtm_read())
             if command and channel and user:
-                bot.handle_command(command, user, channel)
+                bot.handle_command(command, user, channel, is_AT_bot)
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
